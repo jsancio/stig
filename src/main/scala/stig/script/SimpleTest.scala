776356cff052cb34815d6c61f93881d7c5b85468
Jose Armando Garcia Sancio <jsancio@gmail.com>
2014-02-07 11:30:14 -0800

    Implement a sample activity worker

--- src/main/scala/stig/script/SimpleTest.scala
+++ src/main/scala/stig/script/SimpleTest.scala
@@ -2,32 +2,53 @@ package stig.script
 
 import com.amazonaws.services.simpleworkflow.AmazonSimpleWorkflowClient
 
-import stig.{ DecisionManager, DeciderRegistration, DeciderContext, Activity }
+import stig.Activity
+import stig.ActivityManager
+import stig.ActivityRegistration
+import stig.DeciderContext
+import stig.DeciderRegistration
+import stig.DecisionManager
 import stig.model.Workflow
+import stig.WorkerContext
 
 object SimpleTest extends App {
   val client = new AmazonSimpleWorkflowClient()
   client.setEndpoint("swf.us-west-1.amazonaws.com")
 
+  val domain = "test-domain"
+  val taskList = "test_task"
+
   val decisionManager = new DecisionManager(
-    "test-domain",
-    "test_task",
+    domain,
+    taskList,
     client,
     Seq(DeciderRegistration(Workflow("simple-workflow", "1.1"), decider)))
 
+  val activityManager = new ActivityManager(
+    domain,
+    taskList,
+    client,
+    Seq(ActivityRegistration(Activity("simple-activity", "1.0"), activity)))
+
   decisionManager.start()
+  activityManager.start()
   println("Press any key to shutdown...")
   readLine()
+  activityManager.stop()
   decisionManager.stop()
 
   def decider(context: DeciderContext, input: String): Unit = {
     for {
       result <- context.scheduleActivity(
         Activity("simple-activity", "1.0"),
-        "test_list",
+        taskList,
         "cool input")
     } {
       context.completeWorkflow(s"decider's result: $result")
     }
+  }
+
+  def activity(context: WorkerContext, input: String): String = {
+    s"activity result: $input"
   }
 }
